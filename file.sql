ac5a9d7ba028a909b4bee03eeb3cc046538ee216
Neela <roneela@deloitte.com>
2021-03-22 18:10:22 +0530

    first commit

---
+++ file.sql
@@ -0,0 +1,9 @@
+create table abc;
+(
+id int(11) NOT NULL AUTO_INCREMENT,
+Name varchar(128),
+Primary Key(id)
+);
+insert into Instuctor(Name) values('John');
+insert into Instuctor(Name) values('Krystal');
+insert into Instuctor(Name) values('Sam');
